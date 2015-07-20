705a2d8e534416d9c17b32c194ae9591573622d0
matthew.butt@7digital.com <Matthew Butt>
2015-07-20 16:02:06 +0100

    Refactor: tidy up User definition, removing unused stuff.

--- c#/TripServiceKata/TripServiceKata/User/User.cs
+++ c#/TripServiceKata/TripServiceKata/User/User.cs
@@ -3,34 +3,18 @@ using System.Linq;
 
 namespace TripServiceKata.User
 {
-    public class User
-    {
-        private List<Trip.Trip> trips = new List<Trip.Trip>();
-        private List<User> friends = new List<User>();
+	public class User
+	{
+		private readonly List<User> _friends = new List<User>();
 
-        public List<User> GetFriends()
-        {
-            return friends;
-        } 
+		public void AddFriend(User user)
+		{
+			_friends.Add(user);
+		}
 
-        public void AddFriend(User user)
-        {
-            friends.Add(user);
-        }
-
-        public void AddTrip(Trip.Trip trip)
-        {
-            trips.Add(trip);
-        }
-
-        public List<Trip.Trip> Trips()
-        {
-            return trips;
-        }
-
-	    public bool IsFriendsWith(TripServiceKata.User.User loggedUser)
-	    {
-		    return GetFriends().Any(friend => friend == loggedUser);
-	    }
-    }
+		public bool IsFriendsWith(User loggedUser)
+		{
+			return _friends.Any(friend => friend == loggedUser);
+		}
+	}
 }
