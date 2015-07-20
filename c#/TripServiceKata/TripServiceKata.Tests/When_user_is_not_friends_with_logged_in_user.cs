95a0cd3a66b4da871afc3f0b6df559f527e1b8b1
matthew.butt@7digital.com <Matthew Butt>
2015-07-20 15:40:49 +0100

    Test: user is not friends with logged in user.

---
+++ c#/TripServiceKata/TripServiceKata.Tests/When_user_is_not_friends_with_logged_in_user.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
+{
+	[TestFixture]
+	public class When_user_is_not_friends_with_logged_in_user : IUserSession
+	{
+		[Test]
+		public void Returns_no_trips()
+		{
+			var tripService = new TripService(this);
+			var user = new User.User();
+			user.AddTrip(new Trip.Trip());
+			user.AddTrip(new Trip.Trip());
+			user.AddFriend(new User.User());
+			user.AddFriend(new User.User());
+
+			var tripsByUser = tripService.GetTripsByUser(user);
+
+			Assert.That(tripsByUser, Is.Empty);
+		}
+
+		public User.User GetLoggedUser()
+		{
+			return new User.User();
+		}
+	}
+}
